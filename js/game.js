943b80a0facacfd03158810e64e29b8db52a34b6
Denis <denisbormusov@gmail.com>
2024-07-29 21:52:02 +0500

    hardTask

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@
 
 (() => {
   const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
 
   const getRandomIntInclusive = (min, max) => {
     const minCeiled = Math.ceil(min);
@@ -9,24 +10,52 @@
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
   };
 
-  const game = () => {
+  const settingsLanguage = {
+    ru: {
+      player: 'Вы :',
+      computer: 'Компьютер :',
+      win: 'Вы выиграли!',
+      lose: 'Вы проиграли',
+      draw: 'Ничья',
+      score: 'Общий счёт',
+      playAgain: 'Играем ещё?',
+      quit: 'Вы точно хотите выйти ?',
+    },
+    en: {
+      player: 'You :',
+      computer: 'Computer :',
+      win: 'You win!',
+      lose: 'You lost',
+      draw: 'Draw',
+      score: 'Total score',
+      playAgain: 'Are we still playing?',
+      quit: 'Are you sure you want to get out?',
+    },
+  };
+
+  const game = (language) => {
     const result = {
       player: 0,
       computer: 0,
     };
 
-    const getResultMessage = () => alert(`Общий счёт\n` +
-      `Вы : ${result.player}\nКомпьютер : ${result.computer}`);
+    const languageGame = language === 'EN' || language === 'ENG' ? 'en' : 'ru';
+    const figures = languageGame === 'en' ? FIGURES_ENG : FIGURES_RUS;
+    const settingsMessage = settingsLanguage[languageGame];
 
-    const getResultUserChoice = (userEnter) => FIGURES_RUS.find(item =>
+    const getResultMessage = () => alert(`${settingsMessage.score}\n` +
+      `${settingsMessage.player} ${result.player}\n` +
+      `${settingsMessage.computer} ${result.computer}`);
+
+    const getResultUserChoice = (userEnter) => figures.find(item =>
       item.startsWith(userEnter.toLowerCase().trim()));
 
 
     return function start() {
-      const userEnter = prompt(`${FIGURES_RUS} ?`, '');
+      const userEnter = prompt(`${figures} ?`, '');
 
       if (userEnter === null) {
-        if (confirm('Вы точно хотите выйти ?')) {
+        if (confirm(`${settingsMessage.quit}`)) {
           return getResultMessage();
         } else {
           start();
@@ -37,30 +66,35 @@
         return start();
       }
 
-      const computerChoice = FIGURES_RUS[getRandomIntInclusive(0,
-          FIGURES_RUS.length - 1)];
+      const computerChoice = figures[getRandomIntInclusive(0,
+          figures.length - 1)];
       console.log('computerChoice: ', computerChoice);
       const userChoice = getResultUserChoice(userEnter);
-      const userIndex = FIGURES_RUS.indexOf(userChoice);
-      const computerIndex = FIGURES_RUS.indexOf(computerChoice);
+      const userIndex = figures.indexOf(userChoice);
+      const computerIndex = figures.indexOf(computerChoice);
 
       switch (true) {
         case userChoice === computerChoice:
-          alert('Вы: ' +
-            ` ${userChoice}\nКомпьютер: ${computerChoice}\nНичья`);
+          alert(`${settingsMessage.player} ${userChoice}\n` +
+            `${settingsMessage.computer} ${computerChoice}\n` +
+            `${settingsMessage.draw}`);
           break;
-        case (userIndex + 1) % FIGURES_RUS.length === computerIndex:
-          alert('Вы: ' +
-            ` ${userChoice}\nКомпьютер: ${computerChoice}\nВы выиграли!`);
+
+        case (userIndex + 1) % figures.length === computerIndex:
+          alert(`${settingsMessage.player} ${userChoice}\n` +
+            `${settingsMessage.computer} ${computerChoice}\n` +
+            `${settingsMessage.win}`);
           ++result.player;
           break;
+
         default:
-          alert('Вы: ' +
-            ` ${userChoice}\nКомпьютер: ${computerChoice}\nВы проиграли`);
+          alert(`${settingsMessage.player} ${userChoice}\n` +
+            `${settingsMessage.computer} ${computerChoice}\n` +
+            `${settingsMessage.lose}`);
           ++result.computer;
       }
 
-      if (confirm('Играем еще?')) {
+      if (confirm(`${settingsMessage.playAgain}`)) {
         return start();
       } else {
         return getResultMessage();
